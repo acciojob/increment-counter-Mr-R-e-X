3faa069451d49e6ad05aea348c4a0d8e10f541f5
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-26 21:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = 0;
+let P = document.querySelector('#counter')
+P.innerHTML = counter;
+function incrementCounter() {
+	counter+=1;
+	P.innerHTML = counter;
+}
